908a06796ae709f87e4ea384a91e12aa9a2d275e
Your Name <you@example.com>
2021-05-09 00:03:19 -0700

    Added Edit funciton

--- app.py
+++ app.py
@@ -112,9 +112,10 @@ def repairedtable():
     cursor = mysql.connection.cursor()
     cursor.execute("SELECT * FROM inventory where status = 'Repaired' order by DateOrdered desc;")
     data = cursor.fetchall()
+    
     return render_template("repairedtable.html", data=data)
 
-@app.route("/repairedpost", methods=['POST'])
+@app.route("/repairedpost", methods=['POST','GET'])
 def repairedpost():
     DateOrdered = today.strftime("%m/%d/%y")
     change_to_repaired = request.form['change_to_repaired']
@@ -124,6 +125,62 @@ def repairedpost():
     cursor.close()
     flash(change_to_repaired)
     return redirect(request.referrer)
+
+@app.route('/editpost', methods=['POST'])
+def edit():
+    editID = request.form['editID']
+
+    idpull = mysql.connection.cursor()
+    idpull.execute("select id from inventory where id = %s",[editID])
+    idpull = idpull.fetchone()
+
+    servicepull = mysql.connection.cursor()
+    servicepull.execute("select ServiceOrder from inventory where id = %s",[editID])
+    servicepull = servicepull.fetchone()
+
+    unitskupull = mysql.connection.cursor()
+    unitskupull.execute("select UnitSKU from inventory where id = %s",[editID])
+    unitskupull = unitskupull.fetchone()
+
+    partpull = mysql.connection.cursor()
+    partpull.execute("select PartType from inventory where id = %s",[editID])
+    partpull = partpull.fetchone()
+
+    remarkspull = mysql.connection.cursor()
+    remarkspull.execute("select Remarks from inventory where id = %s",[editID])
+    remarkspull = remarkspull.fetchone()
+
+    skupull = mysql.connection.cursor()
+    skupull.execute("select PartSKU from inventory where id = %s",[editID])
+    skupull = skupull.fetchone()
+
+    modelpull = mysql.connection.cursor()
+    modelpull.execute("select Model from inventory where id = %s",[editID])
+    modelpull = modelpull.fetchone()
+    
+    cartpull = mysql.connection.cursor()
+    cartpull.execute("select Cart from inventory where id = %s",[editID])
+    cartpull = cartpull.fetchone()
+    
+    return render_template('edit.html',idpull=idpull[0],servicepull=servicepull[0],unitskupull=unitskupull[0],
+    partpull=partpull[0],remarkspull=remarkspull[0],skupull=skupull[0],modelpull=modelpull[0],cartpull=cartpull[0])
+
+@app.route('/updatepost', methods = ['POST'])
+def update():
+        editID = request.form['editID']
+        ServiceOrder = request.form['ServiceOrder']
+        UnitSKU = request.form['UnitSKU']
+        PartType = request.form['PartType']
+        Remarks = request.form['Remarks']
+        PartSKU = request.form['SKU']
+        Model = request.form['Model']
+        Cart = request.form['Cart']
+        updaterecord = mysql.connection.cursor()
+        updaterecord.execute('''UPDATE inventory set ServiceOrder=%s,UnitSKU=%s,PartType=%s,Remarks=%s,PartSKU=%s,Model=%s,Cart=%s where id = %s''',[ServiceOrder,UnitSKU,PartType,Remarks,PartSKU,Model,Cart,editID])
+        mysql.connection.commit()
+        updaterecord.close()
+        flash(editID)
+        return render_template('edit.html')
 
 app.run(debug=True)
 app.run(host='localhost', port=5000)
